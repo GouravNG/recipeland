e9dcfae9fa1ed4659f359db99c899586214dbc66
Gourav <gouravgunaga555@gmail.com>
2024-08-11 21:03:48 +0530

    added the footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/header'
 import { openSans } from '@/utils/fonts'
+import Footer from '@/components/footer'
 
 export const metadata: Metadata = {
   title: 'RecipeLand',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={openSans.className}>
         <Header />
         {children}
+        <Footer/>
       </body>
     </html>
   )
