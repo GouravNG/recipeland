bb825fa0946009dcc3be545d7ba3476e27720588
Gourav <gouravgunaga555@gmail.com>
2024-08-15 19:51:30 +0530

    Route setup and set those routes with dummy components

---
+++ app/recipe/recipeId/page.tsx
@@ -0,0 +1,8 @@
+const RDP = () => {
+    return (
+      <>
+        <h1>Recipe Desciption page</h1>
+      </>
+    )
+  }
+  export default RDP
--- components/homepageBlocks/Banner.tsx
+++ components/homepageBlocks/Banner.tsx
@@ -4,7 +4,7 @@ const BannerImage = () => {
   return (
     <>
       <div className='flex items-center justify-center my-1 py-2'>
-        <Image src={'/banner.png'} alt='BannerImage' width={1500} height={100} />
+        <Image src={'/banner2.jpg'} alt='BannerImage' width={1500} height={100} />
       </div>
     </>
   )
