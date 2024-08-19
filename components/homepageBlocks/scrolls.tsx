b6629a2dc6bfc0bd46554df9bd601434a4b25253
Gourav <gouravgunaga555@gmail.com>
2024-08-19 20:01:00 +0530

    Refactor:Scroll page

--- components/homepageBlocks/scrolls.tsx
+++ components/homepageBlocks/scrolls.tsx
@@ -1,22 +1,21 @@
 import { IBM_Flex } from '@/utils/fonts'
 import RecipeContainer from '../ui/recipeContainer.component'
 import { MoveRightIcon } from 'lucide-react'
+import { getAllRecipes } from '@/dummyData/getAllRecipe'
+import { RecipeInfo } from '@/types/common.type'
 
 const Scroll = () => {
+  const recipesData: RecipeInfo[] = getAllRecipes
   return (
     <>
       <div className='flex items-center p-4'>
-        <h3 className={`${IBM_Flex.className} p-2 text-3xl underline decoration-yellow-300 decoration-4`}>
-          Explre our recipes
-        </h3>
+        <h3 className={`${IBM_Flex.className} p-2 subHeadingStyle underLineDecoration`}>Explre our recipes</h3>
         <MoveRightIcon />
       </div>
       <div className='bg-slate-300 flex gap-8 p-4 py-8 overflow-x-auto overflow-y-hidden whitespace-nowrap'>
-        <RecipeContainer width={300} />
-        <RecipeContainer width={300} />
-        <RecipeContainer width={300} />
-        <RecipeContainer width={300} />
-        <RecipeContainer width={300} />
+        {recipesData.map((recipe, index) => {
+          return <RecipeContainer recipeInfo={recipe} key={index} />
+        })}
       </div>
     </>
   )
--- components/ui/recipeContainer.component.tsx
+++ components/ui/recipeContainer.component.tsx
@@ -1,30 +1,34 @@
-import { ForkKnifeCrossed, MessageCircle, Star, StarIcon } from 'lucide-react'
+import { RecipeInfo } from '@/types/common.type'
 import Image from 'next/image'
+import FavouriteComponent from './favourite.component'
+import { Star } from 'lucide-react'
+import Link from 'next/link'
 
-const RecipeContainer = ({ width = 210 }) => {
+type RecipeContainerProps = {
+  recipeInfo: RecipeInfo
+  width?: number
+}
+
+const RecipeContainer: React.FC<RecipeContainerProps> = ({ width = 240, recipeInfo }) => {
   return (
     <>
-      <div className='flex-shrink-0 shadow-md'>
-        <div className='relative flex items-end '>
-          <Image src={'/cheese.jpg'} alt='cheese' width={width} height={50} />
-          <p className='absolute p-1 bg-slate-100 m-1 font-bold text-sm rounded-sm'>Desert</p>
-        </div>
-        <div className='flex flex-col p-1 bg-white'>
-          <div className='flex items-center justify-between px-2'>
-            <div className='flex items-center'>
-              <MessageCircle fill='true' size='20' />
-              <p>10</p>
-            </div>
-            <div className='flex items-center'>
-              <StarIcon fill='true' size='20' />
-              <p>10</p>
-            </div>
+      <Link href={`recipe/${recipeInfo?.recipeName}`}>
+        <div className='flex-shrink-0  w-60 p-1 bg-white hoverAnimation'>
+          <div className='relative flex flex-col justify-end'>
+            <p className='absolute p-1 bg-slate-100 m-1 font-bold text-sm rounded-sm italic'>
+              {recipeInfo?.recipeCategory}
+            </p>
+            <Image src={recipeInfo?.recipeMainImage} alt={recipeInfo?.recipeMainImageAlt} width={width} height={50} />
           </div>
-          <div className='pt-2'>
-            <h3 className='font-bold'>Straberry Cheese Cake</h3>
+          <div className='flex flex-col p-1.5 bg-white '>
+            <p className='font-serif text-lg'>{recipeInfo?.recipeName}</p>
+            <p className='flex place-items-center gap-1'>
+              {recipeInfo?.recipeRating}
+              <Star fill='gold' strokeWidth={'.5px'} size={'15px'} />
+            </p>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   )
 }
