9b2e341216a2db927662128855250381550b2fdd
Gourav <gouravgunaga555@gmail.com>
2024-08-31 17:55:48 +0530

    Added the instruction and Misc part

--- components/createRecipeBlocks/instructions.component.tsx
+++ components/createRecipeBlocks/instructions.component.tsx
@@ -1,5 +1,48 @@
+import { useState } from 'react'
+import { Button } from '../ui/button'
+
 const Instructions = () => {
-  return <h1>Instructions</h1>
+  const [step, setStep] = useState<number>(1)
+  return (
+    <div className=' p-1 gap-1'>
+      <p className=' text-xl p-1'>Add Step</p>
+      <ol className='bg-yellow-100 w-full flex flex-col gap-2 items-center justify-between p-1'>
+        {Array.from({ length: step }).map((_, index) => {
+          return (
+            <div className='w-full' key={index}>
+              <p className='w-full'>{` Step: ${index + 1}`}</p>
+              <li className='w-full flex gap-1 '>
+                <input
+                  type='text'
+                  className='bg-yellow-50 w-11/12 p-1 outline-none border-b-2 border-black'
+                  name={'Step' + index}
+                  id={'Step' + index}
+                />
+                <Button
+                  className='w-1/12'
+                  onClick={(e) => {
+                    console.log('need to handle this click')
+                    e.preventDefault()
+                  }}
+                >
+                  Add img
+                </Button>
+                <Button
+                  className='w-1/12'
+                  onClick={(e) => {
+                    setStep(step + 1)
+                    e.preventDefault()
+                  }}
+                >
+                  Next Step
+                </Button>
+              </li>
+            </div>
+          )
+        })}
+      </ol>
+    </div>
+  )
 }
 
 export default Instructions
--- components/createRecipeBlocks/misc.component.tsx
+++ components/createRecipeBlocks/misc.component.tsx
@@ -1,5 +1,44 @@
 const Misc = () => {
-  return <h1>Misc</h1>
+  return (
+    <div className=' h-40 flex p-1 w-full justify-around items-center '>
+      <div className=' bg-yellow-200 flex flex-col h-full justify-around items-center p-1 border rounded-md shadow-md'>
+        <label htmlFor='' className='text-3xl font-bold'>
+          Servings
+        </label>
+        <input
+          type='text'
+          name=''
+          id=''
+          className='outline-none border-b-2 border-black bg-transparent text-center w-1/2 text-xl'
+          placeholder="Persons"
+        />
+      </div>
+      <div className=' bg-yellow-200 flex flex-col h-full justify-around items-center p-2 border rounded-md shadow-md'>
+        <label htmlFor='' className='text-3xl font-bold'>
+          Preperation Time
+        </label>
+        <input
+          type='text'
+          name=''
+          id=''
+          className='outline-none border-b-2 border-black bg-transparent text-center w-1/2 text-xl'
+          placeholder="Min/Hr"
+        />
+      </div>
+      <div className=' bg-yellow-200 flex flex-col h-full justify-around items-center p-1 border rounded-md shadow-md'>
+        <label htmlFor='' className='text-3xl font-bold'>
+          Cooking Time
+        </label>
+        <input
+          type='text'
+          name=''
+          id=''
+          className='outline-none border-b-2 border-black bg-transparent text-center w-1/2 text-xl'
+          placeholder="Min/Hr"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Misc
